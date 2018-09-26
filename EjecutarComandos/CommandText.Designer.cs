af6393df5247c172e527f52e7f0827efc0a8a770
Gimena_UNICAH <Gimena_UNICAH@GIMENA>
2018-09-26 10:49:17 -0600

    Crreaccion del formulario CommandText

---
+++ EjecutarComandos/CommandText.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EjecutarComandos
+{
+    partial class CommandText
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCommandText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCommandText
+            // 
+            this.txtCommandText.Location = new System.Drawing.Point(25, 12);
+            this.txtCommandText.Multiline = true;
+            this.txtCommandText.Name = "txtCommandText";
+            this.txtCommandText.Size = new System.Drawing.Size(358, 140);
+            this.txtCommandText.TabIndex = 0;
+            // 
+            // CommandText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 181);
+            this.Controls.Add(this.txtCommandText);
+            this.Name = "CommandText";
+            this.Text = "Ejecutar Comandos";
+            this.Load += new System.EventHandler(this.CommandText_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCommandText;
+    }
+}
+
---
+++ EjecutarComandos/CommandText.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace EjecutarComandos
+{
+    public partial class CommandText : Form
+    {
+        public CommandText()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandText_Load(object sender, EventArgs e)
+        {
+            //Crear la conexion
+            SqlConnection conn = new SqlConnection(@"server = (local\)\sqlexpress;
+                integrated security = true; database = AdventureWorks2014");
+            //crear el comando
+            SqlCommand cmd = new SqlCommand();
+
+            try{
+                //establecer la conexion
+                conn.Open();
+                txtCommandText.AppendText("Conexion Establecida\n");
+                txtCommandText.AppendText("Comando creado. \n");
+
+                //Estableciendo el valor del comando
+                cmd.CommandText = @"SELECT Name, ProductNumber
+                                    FROM Production.product";
+                txtCommandText.AppendText("Listo paa la ejecucion del query SQL: \n\t\t" + cmd.CommandText);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace, "Detalle de la excepcion");
+            }
+            finally
+            {
+                //cerrar conexion
+                conn.Close();
+                txtCommandText.AppendText("\n Conexion Cerrada");
+            }
+        }
+    }
+}
