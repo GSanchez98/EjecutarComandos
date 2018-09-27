9ef90d5887294d4c2d590483fe5efcd29aae8b95
Gimena_UNICAH <Gimena_UNICAH@GIMENA>
2018-09-27 10:50:02 -0600

    comienzo del formulario ExecuteReader

---
+++ EjecutarComandos/ExecuteReader.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace EjecutarComandos
+{
+    partial class ExecuteReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtExecuteReader = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtExecuteReader
+            // 
+            this.txtExecuteReader.Location = new System.Drawing.Point(12, 12);
+            this.txtExecuteReader.Multiline = true;
+            this.txtExecuteReader.Name = "txtExecuteReader";
+            this.txtExecuteReader.Size = new System.Drawing.Size(454, 235);
+            this.txtExecuteReader.TabIndex = 0;
+            // 
+            // ExecuteReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 259);
+            this.Controls.Add(this.txtExecuteReader);
+            this.Name = "ExecuteReader";
+            this.Text = "ExecuteReader";
+            this.Load += new System.EventHandler(this.ExecuteReader_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtExecuteReader;
+    }
+}
---
+++ EjecutarComandos/ExecuteReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace EjecutarComandos
+{
+    public partial class ExecuteReader : Form
+    {
+        public ExecuteReader()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteReader_Load(object sender, EventArgs e)
+        {
+            //Crear la conexion
+            SqlConnection conn = new SqlConnection(@"server = (local\)\sqlexpress;
+                integrated security = true; database = AdventureWorks2014");
+
+            //crear el query 
+            string sql = @"SELECT Name, ProductNumber
+                           FROM Production.Product";
+
+            //crear el comando, primero comando luego conexion
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            try
+            {
+                /*establecer la conexion
+                conn.Open();
+                txtExecuteReader.AppendText("Comando Creado, Conexion Establecida\n");
+                txtExecuteReader.AppendText("el numero de productos en Producion.Product es:");
+
+                //ejecutar el query escalar
+                txtExecuteReader.AppendText(cmd.ExecuteScalar().ToString());
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace, "Detalle del error");
+            }
+            finally
+            {
+                //cerrar conexion
+                conn.Close();
+                txtEscalar.AppendText("\n Conexion Cerrada");
+            }
+        }
+    }
+}
